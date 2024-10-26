d37796bb9ef314c45f23a5670d41bededed37221
David Houston <davidaehouston@gmail.com>
2024-10-26 14:51:35 +0100

    Extending repo with Jpa, adding basic service Methods

--- backend/src/main/java/dh/workfromhere/spaces/repository/SpaceResourceRepository.java
+++ backend/src/main/java/dh/workfromhere/spaces/repository/SpaceResourceRepository.java
@@ -1,4 +1,7 @@
 package dh.workfromhere.spaces.repository;
 
-public interface SpaceResourceRepository {
+import dh.workfromhere.spaces.model.SpaceResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpaceResourceRepository extends JpaRepository<SpaceResource, Integer> {
 }
--- backend/src/main/java/dh/workfromhere/spaces/service/SpaceResourceService.java
+++ backend/src/main/java/dh/workfromhere/spaces/service/SpaceResourceService.java
@@ -1,24 +1,52 @@
 package dh.workfromhere.spaces.service;
 
 import dh.workfromhere.spaces.model.SpaceResource;
+import dh.workfromhere.spaces.repository.SpaceResourceRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SpaceResourceService {
+
+    private final SpaceResourceRepository spaceResourceRepository;
+
+    public SpaceResourceService(SpaceResourceRepository spaceResourceRepository) {
+        this.spaceResourceRepository = spaceResourceRepository;
+    }
+
     public List<SpaceResource> getAllSpaces() {
+        return spaceResourceRepository.findAll();
     }
 
     public SpaceResource getSpaceById(Integer id) {
+        return spaceResourceRepository.findById(id).orElse(null);
     }
 
     public SpaceResource createSpace(SpaceResource spaceResource) {
+        return spaceResourceRepository.save(spaceResource);
     }
 
     public void updateSpaceById(Integer id, SpaceResource spaceResource) {
+        SpaceResource spaceResourceToUpdate = spaceResourceRepository.findById(id).orElse(null);
+
+        if (spaceResourceToUpdate != null) {
+            spaceResourceToUpdate.setName(spaceResource.getName());
+            spaceResourceToUpdate.setDescription(spaceResource.getDescription());
+            spaceResourceToUpdate.setType(spaceResource.getType());
+            spaceResourceToUpdate.setWifiAvailable(spaceResource.getWifiAvailable());
+            spaceResourceToUpdate.setWifiPassword(spaceResource.getWifiPassword());
+            spaceResourceToUpdate.setWifiSpeed(spaceResource.getWifiSpeed());
+
+            spaceResourceRepository.save(spaceResourceToUpdate);
+        }
     }
 
     public void deleteSpaceById(Integer id) {
+        SpaceResource spaceResourceToDelete = spaceResourceRepository.findById(id).orElse(null);
+
+        if (spaceResourceToDelete != null) {
+            spaceResourceRepository.delete(spaceResourceToDelete);
+        }
     }
 }
