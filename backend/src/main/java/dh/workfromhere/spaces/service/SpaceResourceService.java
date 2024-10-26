063d5490b779a0942d63d25c912bec9052fdf22f
David Houston <davidaehouston@gmail.com>
2024-10-26 14:48:07 +0100

    Adding skeleton service classes

--- backend/src/main/java/dh/workfromhere/spaces/service/SpaceResourceService.java
+++ backend/src/main/java/dh/workfromhere/spaces/service/SpaceResourceService.java
@@ -1,7 +1,24 @@
 package dh.workfromhere.spaces.service;
 
+import dh.workfromhere.spaces.model.SpaceResource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SpaceResourceService {
+    public List<SpaceResource> getAllSpaces() {
+    }
+
+    public SpaceResource getSpaceById(Integer id) {
+    }
+
+    public SpaceResource createSpace(SpaceResource spaceResource) {
+    }
+
+    public void updateSpaceById(Integer id, SpaceResource spaceResource) {
+    }
+
+    public void deleteSpaceById(Integer id) {
+    }
 }
