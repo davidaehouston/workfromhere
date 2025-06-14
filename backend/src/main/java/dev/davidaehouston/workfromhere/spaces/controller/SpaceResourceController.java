8276294f12f47649ce291a37450a6c66c251ae5b
David Houston <davidaehouston@gmail.com>
2025-06-14 20:34:52 +0100

    Skeleton classes for controller, model, repo & service layers

---
+++ backend/src/main/java/dev/davidaehouston/workfromhere/spaces/controller/SpaceResourceController.java
@@ -0,0 +1,7 @@
+package dev.davidaehouston.workfromhere.spaces.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpaceResourceController {
+}
